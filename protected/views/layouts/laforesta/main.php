5a189642846727b0edc8bf38556581f2a3c85769
dachaga <dachaga@summasolutions.net>
2015-03-29 11:33:18 -0300

    mockup home

--- protected/views/layouts/laforesta/main.php
+++ protected/views/layouts/laforesta/main.php
@@ -46,9 +46,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+
 	</div><!-- footer -->
 
 </div><!-- page -->
---
+++ protected/views/site/laforesta/index.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+<div id="center_box_home">
+    <a href="<?php echo Yii::app()->request->baseUrl.'?r=insumos'; ?>">
+        <img width="720" src="<?php echo Yii::app()->request->baseUrl; ?>/images/home_image.jpg" alt="main_la_foresta"/>
+        <p>Cálculo rapido</p>
+    </a>
+</div>
